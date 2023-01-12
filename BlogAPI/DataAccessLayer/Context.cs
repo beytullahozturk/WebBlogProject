25fbcfb216103146ed30c99a91ca3ee87d660a8c
Beytullah Öztürk <ozbeytullah2@outlook.com>
2023-01-12 18:09:22 +0300

    web api added

---
+++ BlogAPI/DataAccessLayer/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogAPI.DataAccessLayer
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=MonsterPC\SQLEXPRESS; Database=BlogWebApiDb; Trusted_Connection=True");
+        }
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
